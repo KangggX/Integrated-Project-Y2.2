fe1b75005b052ba72a559b0dd09ba2cf5e96c391
KangggX <lowkangxuan@gmail.com>
2022-01-28 01:19:55 +0800

    testing target movement

--- Unity/Assets/Scripts/Kang/TargetMovement.cs
+++ Unity/Assets/Scripts/Kang/TargetMovement.cs
@@ -6,6 +6,9 @@ public class TargetMovement : MonoBehaviour
 {
     [SerializeField] private float _transitionSpeed;
     [SerializeField] private Transform _inPosition;
+    [SerializeField] private Transform _outPosition;
+
+    public bool canMove;
     [SerializeField] private bool _isOut;
 
     private Vector3 _basePosition;
@@ -18,12 +21,37 @@ public class TargetMovement : MonoBehaviour
 
     private void Update()
     {
-        if (_isOut)
+        if (_isOut && canMove)
         {
             _elapsedTime += Time.deltaTime;
             float percentageCompletion = _elapsedTime / _transitionSpeed;
 
             gameObject.transform.position = Vector3.Lerp(_basePosition, _inPosition.position, percentageCompletion);
+
+            if (gameObject.transform.position == _inPosition.position)
+            {
+                _isOut = false;
+                canMove = false;
+                _elapsedTime = 0;
+
+                _basePosition = gameObject.transform.position;
+            }
+        }
+        else if (!_isOut && canMove)
+        {
+            _elapsedTime += Time.deltaTime;
+            float percentageCompletion = _elapsedTime / _transitionSpeed;
+
+            gameObject.transform.position = Vector3.Lerp(_basePosition, _outPosition.position, percentageCompletion);
+
+            if (gameObject.transform.position == _outPosition.position)
+            {
+                _isOut = true;
+                canMove = false;
+                _elapsedTime = 0;
+
+                _basePosition = gameObject.transform.position;
+            }
         }
     }
 }
