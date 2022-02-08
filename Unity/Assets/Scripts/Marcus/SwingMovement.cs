af6e43680488892440123cd08a2ce45ae4d8830a
LeeKaMengMarcus <s10204446@connect.np.edu.sg>
2022-02-08 12:26:56 +0800

    implementing ski movement

--- Unity/Assets/Scripts/Marcus/SwingMovement.cs
+++ Unity/Assets/Scripts/Marcus/SwingMovement.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SwingMovement : MonoBehaviour
 {
-
     //gameobjects
     public GameObject leftHand;
     public GameObject rightHand;
     public GameObject cameraCenter;
     public GameObject forwardDirection;
+
+    //character controller
+    private CharacterController cController;
 
     //Vector 3
     private Vector3 positionPreviousFrameLeftHand;
@@ -21,11 +22,25 @@ public class SwingMovement : MonoBehaviour
 
     //speed
     public float speed = 70;
+    [SerializeField]
     private float handSpeed;
+
+    public float playerAcceleration;
+    public float skiVelocity;
+    public float dragForce;
+
+    //check if Skiing
+    [SerializeField]
+    private bool isSki;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        dragForce = 1;
+        cController = GetComponent<CharacterController>();
+
         //set previous frame
         playerPositionPreviousFrame = transform.position;
         positionPreviousFrameLeftHand = leftHand.transform.position;
@@ -45,6 +60,7 @@ public class SwingMovement : MonoBehaviour
         forwardDirection.transform.eulerAngles = new Vector3(0, yRotation, 0);
     }
 
+    //Walk Motion
     private void PlayerPosition()
     {
         //Hands
@@ -59,13 +75,35 @@ public class SwingMovement : MonoBehaviour
         var leftHandDistance = Vector3.Distance(positionPreviousFrameLeftHand, positionThisFrameLeftHand);
         var rightHandDistance = Vector3.Distance(positionPreviousFrameRightHand, positionThisFrameRightHand);
 
+        Vector3 velocity = cController.velocity;
+
+        skiVelocity = playerDistanceMoved / 1f;
+
         //get hand speed
         handSpeed = ((leftHandDistance - playerDistanceMoved) + (rightHandDistance - playerDistanceMoved));
 
         //making player move
         if (Time.timeSinceLevelLoad > 1f)
         {
-            transform.position += forwardDirection.transform.forward * handSpeed * speed * Time.deltaTime;
+            //apply movement type
+            if(isSki == true)
+            {
+;
+                //Warning: change in framerate may cause change in speed
+                velocity += forwardDirection.transform.forward * playerAcceleration  * handSpeed;
+
+                velocity *= dragForce/2;
+
+                cController.Move(velocity * 10 * Time.deltaTime);
+            }
+            else
+            {
+                velocity += forwardDirection.transform.forward * playerAcceleration * handSpeed;
+
+                velocity *= dragForce;
+
+                cController.Move(((velocity) * Time.deltaTime));
+            }
         }
 
         //setting previous position
@@ -73,5 +111,22 @@ public class SwingMovement : MonoBehaviour
         positionPreviousFrameRightHand = positionThisFrameRightHand;
 
         playerPositionPreviousFrame = playerPositionThisFrame;
+    }
+
+    public void SnowIntereact()
+    {
+        RaycastHit Hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit))
+        {
+            if (gameObject.tag == "snow")
+            {
+                isSki = true;
+            }
+        }
+    }
+
+    public void SnowRelease()
+    {
+        isSki = false;
     }
 }
