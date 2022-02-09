e322fe78f3e1086c12bf7d7986e8efb85166d19b
S10206112 <73154832+S10206112@users.noreply.github.com>
2022-02-09 11:58:36 +0800

    Outdoor Shooting Game Added

---
+++ Unity/Assets/Scripts/Dan/OutSniper.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
+using TMPro;
+
+public class OutSniper : OutWeapon
+{
+    [SerializeField]
+    private Projectile bulletPrefab;
+
+    public TextMeshProUGUI DebugText;
+    public GameObject magazine;
+    protected override void StartShooting(ActivateEventArgs interactor)
+    {
+        base.StartShooting(interactor);
+        Shoot();
+        
+        
+    }
+    
+
+    protected override void Shoot()
+    {
+        if(base.canShootAmmo == true)
+        {
+            base.Shoot();
+            Projectile projectileInstantance = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            projectileInstantance.Init(this);
+            projectileInstantance.Launch();
+            Debug.Log("Bullet has been shot");
+            DebugText.text = "Sniper : Shot";
+
+
+            magazine.transform.parent = null;
+            magazine.AddComponent<Rigidbody>();
+        }
+
+        else
+        {
+            DebugText.text = "Sniper : Cooldown";
+        }
+        
+    }
+
+        
+    
+
+    protected override void StopShooting(DeactivateEventArgs interactor)
+    {
+        base.StopShooting(interactor);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - base.StartTime > base.shootingCooldown)
+            {
+                base.canShootAmmo = true;
+                Debug.Log("Finished Cooldown");
+                DebugText.text = "Sniper : Ready";
+            }
+    }
+}
