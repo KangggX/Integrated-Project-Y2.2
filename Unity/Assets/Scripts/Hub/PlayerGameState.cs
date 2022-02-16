d516faf454911cf6d5efcc3236afa7b707f3b4e3
KangggX <lowkangxuan@gmail.com>
2022-02-16 21:29:43 +0800

    working on arcade machine and profile menu

--- Unity/Assets/Scripts/Hub/PlayerGameState.cs
+++ Unity/Assets/Scripts/Hub/PlayerGameState.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerEnterState : MonoBehaviour
+public class PlayerGameState : MonoBehaviour
 {
     public static bool HasEnteredBefore = false;
+
+    public static bool CanPlaySkiing;
+    public static bool CanPlayIndoorShooting = true;
+    public static bool CanPlayOutdoorShooting;
 
     private void Awake()
     {
